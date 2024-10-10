5ca663c4a36a0e3c5e642d3ccf2e7c7122563a1d
Xavier Cooper <xcc2b@virginia.edu>
2024-10-10 17:25:37 +0000

    clean up +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,5 @@
 <script>
-	import App from '$lib/components/App.svelte';
 </script>
 
-<div id="background-container">
-	<App />
-</div>
-
 <style>
-	:global(body) {
-		margin: 0;
-		overflow: hidden; /* Prevent default scrolling */
-	}
-
-	#background-container {
-		position: fixed;
-		z-index: -1;
-		border: none;
-		pointer-events: none;
-		left: 0;
-		top: 0;
-		width: 100vw;
-		height: 100vh;
-		background: rgb(13, 19, 32);
-		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
-	}
 </style>
